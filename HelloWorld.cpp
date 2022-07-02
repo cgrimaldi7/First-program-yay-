4cac61859613e55ca89e38f7de8c44ad68c8a35a
Claudia Grimaldi <107763630+cgrimaldi7@users.noreply.github.com>
2022-07-01 22:06:16 -0400

    Update HelloWorld.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -4,6 +4,6 @@ int main()
 {
 	cout << "Hello World!\n";
 	cout << "My name is Claudia."<<endl;
-	cout << "Today is July 1st, 2022. This is my first program. Although it's very basic, I am still very proud of it hehe.";
+	cout << "Today is July 1st, 2022. This is my first program. Although it's very basic, but I am still very proud of it hehe.";
 	system("pause>0");
 }
